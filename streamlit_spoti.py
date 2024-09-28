f766123c2caa2259192e041e33d03b148faac74e
me@example.com <me@example.com>
2024-09-28 16:53:33 -0400

    added format to application

--- streamlit_spoti.py
+++ streamlit_spoti.py
@@ -20,15 +20,38 @@ def get_nearest_neighbors(df, nn_model, scaler, query_index, n_neighbors=3):
     return list(zip(neighbor_ids, neighbor_distances))
 
 
-st.write("# Spotify Recommendation System")
+st.title("Spotify Recommendation System")
 
-artist_name = st.text_input('Enter Artist name')
-song_name = st.text_input('Enter Song name')
+st.markdown("""
+ * Use the menu at left to select the artist and song you'd like recommendations for.
+ * Results and additional plots will appear below.
+""")
+
+songs_df = pd.read_csv('1001_artists_discography.csv', index_col=0)
+songs_df.index = songs_df.index.astype(str)
+
+st.sidebar.markdown("## Select Artist and Song")
+
+# list of options for autocomplete-like functionality
+artist_options = sorted(songs_df.artist.unique())   
+default_option = ''  # blank default option
+# prepend the default blank option to the options list
+artist_options = [default_option] + artist_options
+
+# display a selectbox where users can choose from predefined options
+artist_name = st.sidebar.selectbox('Artist:', artist_options)
+
+if artist_name == default_option:
+    song_options = sorted(songs_df.index.unique())
+else:
+    song_options = sorted(songs_df[songs_df.artist == artist_name].index.unique())
+
+song_options = [default_option] + song_options
+song_name = st.sidebar.selectbox('Song:', song_options)
 
 
-if st.button('Predict'):
+if st.sidebar.button('Predict'):
 
-    songs_df = pd.read_csv('related_artists_discography_first_level_p1.csv', index_col=0)
     # Example usage
     # song_name = 'anything'
     # artist_name = 'Adrianne Lenker'
@@ -60,7 +83,7 @@ if st.button('Predict'):
     nn.fit(X_scaled)
 
     # query_index = 'Walk In The Park - Beach House'
-    neighbors = get_nearest_neighbors(songs_df_prep, nn, scaler, query_index, n_neighbors=10)
+    neighbors = get_nearest_neighbors(songs_df_prep, nn, scaler, query_index, n_neighbors=20)
 
     # print(f"The nearest neighbors for {query_index} are:")
     # for neighbor_id, distance in neighbors:
@@ -68,9 +91,26 @@ if st.button('Predict'):
 
 
 
-    st.write(f'<p class="big-font">The nearest neighbors for {query_index} are:</p>', unsafe_allow_html=True)
+    # st.write(f'<p class="big-font">The nearest neighbors for {query_index} are:</p>', unsafe_allow_html=True)
+    
+    col1, col2 = st.columns([3,1])
+    col1.markdown('#### Song')
+    col2.markdown('#### Distance')
+    # col1.write('Song')
+    # col2.write('Distance')
+
+    # # Three columns with different widths
+    # col1, col2, col3 = st.columns([3,1,1])
+    # # col1 is wider
+
+    # # Using 'with' notation:
+    # >>> with col1:
+    # >>>     st.write('This is column 1')
+    
     for neighbor_id, distance in neighbors:
-        st.write(f"Song: {neighbor_id}, Distance: {round(distance,2)}")
+        col1.write(neighbor_id)
+        col2.write(round(distance,2))
+        # st.write(f"Song: {neighbor_id}, Distance: {round(distance,2)}")
 
 
 
